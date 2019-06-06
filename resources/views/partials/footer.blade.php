0b6a756c42a5e4b88890e69759ae770305d81137
MatthewBlurton <mjblurton@hotmail.com>
2019-06-06 10:19:50 +0800

    Modified web layout
    Created partials to include pre-configured blade layouts
    
    Modified the app layout to use partials instead so that multiple layouts can include different partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3">
+    <div class="container">
+        <span class="text-muted">Copyright CrossLink<i class="fa fa-copyright" aria-hidden="true"></i> 2019</span>
+    </div>
+</footer>
